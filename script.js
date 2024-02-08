3ec2f7c1eb357c2ac73bcc3fa23bff236421c75d
shubham-cj <119930498+shubham-cj@users.noreply.github.com>
2024-02-08 10:32:44 +0530

    Frontend Part Complete

---
+++ script.js
@@ -0,0 +1,5 @@
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+// ea80318c644d129973c0fe09bde22fee
+
+// https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=ea80318c644d129973c0fe09bde22fee
